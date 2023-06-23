8e10a646281c65df695829f5c9b480a0be061a2a
Rishav Gupta <gupta.rishav2608@gmail.com>
2023-06-23 07:06:03 +0530

    Initial Commit

---
+++ InstafilterLearn/InstafilterLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterLearnApp.swift
+//  InstafilterLearn
+//
+//  Created by Rishav Gupta on 23/06/23.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
