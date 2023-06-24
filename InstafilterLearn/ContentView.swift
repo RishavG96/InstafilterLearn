f1c0fcdb73488abdd4621eac51805be1fc6c27ac
Rishav Gupta <gupta.rishav2608@gmail.com>
2023-06-24 08:25:03 +0530

    Core Image integrated

--- InstafilterLearn/ContentView.swift
+++ InstafilterLearn/ContentView.swift
@@ -5,6 +5,8 @@
 //  Created by Rishav Gupta on 23/06/23.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
@@ -27,32 +29,97 @@ struct ContentView: View {
     @State private var showingConfirmation = false
     @State private var backgroundColor = Color.white
     
+    
+    @State private var image: Image?
+    
     var body: some View {
+//        VStack {
+//            Text("Hello World!")
+//                .blur(radius: blurAmount)
+//                .frame(width: 300, height: 300)
+//                .background(backgroundColor)
+//                .onTapGesture {
+//                    showingConfirmation = true
+//                }
+//
+//            Slider(value: $blurAmount, in: 0...20)
+//
+//            Button("Random Blur") {
+//                blurAmount = Double.random(in: 0...20)
+//            }
+//        }
+//        .onChange(of: blurAmount) { newValue in
+////            print("New value is \(newValue)")
+//        }
+//        .confirmationDialog("Change Background", isPresented: $showingConfirmation) {
+//            Button("Red") { backgroundColor = .red }
+//            Button("Green") { backgroundColor = .green }
+//            Button("Blue") { backgroundColor = .blue }
+//            Button("Cancel", role: .cancel) { }
+//        } message: {
+//            Text("Select a new color")
+//        }
+        
+        // Day 63
+        
         VStack {
-            Text("Hello World!")
-                .blur(radius: blurAmount)
-                .frame(width: 300, height: 300)
-                .background(backgroundColor)
-                .onTapGesture {
-                    showingConfirmation = true
-                }
-            
-            Slider(value: $blurAmount, in: 0...20)
-            
-            Button("Random Blur") {
-                blurAmount = Double.random(in: 0...20)
-            }
+            image?
+                .resizable()
+                .scaledToFit()
         }
-        .onChange(of: blurAmount) { newValue in
-//            print("New value is \(newValue)")
+        .onAppear(perform: loadImage)
+    }
+    
+    // UIKit ->
+    // 1. UIView 2. UIViewController 3. Delegation
+    
+    
+    func loadImage() {
+//        image = Image("Example") // Image is not good with working with pixel data
+        
+        // UIImage -> comes from UIKit, closest alternative to SwiftUI Image
+        // CGImage -> comes for Core Graphics, 2D array of pizels. UIImage adds a lot of funcitonaly on top of CGImage
+        // CIImage -> comes from core Image, stores all the information required to produce an image. Does not turn it into pixels unless called to do so.
+        
+        // Can be created with:
+        // CGImage <--> UIImage
+        // CIImage --> UIImage
+        // CGImage <--> CIImage
+        // swiftui image --> UIImage, CGImage
+        
+        guard let inputImage = UIImage(named: "Example") else { return }
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+//        let currentFilter = CIFilter.sepiaTone()
+//        let currentFilter = CIFilter.pixellate()
+//        let currentFilter = CIFilter.crystallize()
+        let currentFilter = CIFilter.twirlDistortion()
+        currentFilter.inputImage = beginImage
+//        currentFilter.intensity = 1
+//        currentFilter.scale = 100
+//        currentFilter.radius = 200
+//        currentFilter.radius = 1000
+//        currentFilter.center = CGPoint(x: inputImage.size.width / 2, y: inputImage.size.height / 2)
+        
+        let amount = 1.0
+        let inputKeys = currentFilter.inputKeys
+        
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(amount, forKey: kCIInputIntensityKey)
         }
-        .confirmationDialog("Change Background", isPresented: $showingConfirmation) {
-            Button("Red") { backgroundColor = .red }
-            Button("Green") { backgroundColor = .green }
-            Button("Blue") { backgroundColor = .blue }
-            Button("Cancel", role: .cancel) { }
-        } message: {
-            Text("Select a new color")
+        if inputKeys.contains(kCIInputRadiusKey) {
+            currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey)
+        }
+        if inputKeys.contains(kCIInputScaleKey) {
+            currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey)
+        }
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        
+        if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgImage)
+            image = Image(uiImage: uiImage)
         }
     }
 }
